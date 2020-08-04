1449541d4a87a9281cd17f9429a0c099d1d4b36e
xLasercut <tianxun.wang2004@gmail.com>
2020-08-04 16:43:04 +0100

    added tf s3 backend state

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">=0.12.8"
+  backend "s3" {
+    encrypt        = true
+    bucket         = "anime-quiz-state-bucket"
+    key            = "terraform.tfstate"
+    dynamodb_table = "anime-quiz-state-table"
+    region         = "eu-west-1"
+  }
+}
+
 provider "aws" {
   region = var.aws-region
 }
