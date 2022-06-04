caf9d41c71a99450fc2faa1851c2dbcfbaaefcb9
xLasercut <tianxun.wang2004@gmail.com>
2022-06-04 20:56:02 +0100

    aaded more songs

--- server/src/helpers.ts
+++ server/src/helpers.ts
@@ -6,9 +6,9 @@ function isGameRoom(roomId: string): boolean {
 }
 
 function shuffleSongList(songList: AqSong[]): AqSong[] {
-  const shuffledList = songList
+  const shuffledList = [].concat(songList)
   let currentIndex = shuffledList.length, randomIndex
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex)
     currentIndex--;
     [ shuffledList[currentIndex], shuffledList[randomIndex] ] = [
