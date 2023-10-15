91f91c446320b2e5fe19564692ce864ba7b6e981
xLasercut <tianxun.wang2004@gmail.com>
2023-10-15 14:15:41 +0100

    added game score

--- anime-quiz-client/src/components/main-game/MainGameWindow.vue
+++ anime-quiz-client/src/components/main-game/MainGameWindow.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-col cols="7" md="8">test</v-col>
+  <v-col cols="7" md="8">
+    <main-game-window-answer-display></main-game-window-answer-display>
+  </v-col>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainGameWindowAnswerDisplay from "@/components/main-game/main-game-window/MainGameWindowAnswerDisplay.vue";
+</script>
---
+++ anime-quiz-client/src/components/main-game/main-game-window/MainGameWindowAnswerDisplay.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-row no-gutters justify="center">
+        <v-col cols="5" sm="3" md="2">
+          <v-sheet class="count-container"> 0 / 0</v-sheet>
+        </v-col>
+      </v-row>
+      <v-row no-gutters justify="center">
+        <v-col cols="12" sm="8" md="6" lg="5">
+          <component :is="answerDisplay()"></component>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import MainAnswerAnswerDisplay from '@/components/main-game/main-game-window/main-game-window-answer-display/MainAnswerAnswerDisplay.vue';
+import { ref } from 'vue';
+import MainAnswerQuestionDisplay from '@/components/main-game/main-game-window/main-game-window-answer-display/MainAnswerQuestionDisplay.vue';
+
+const show = ref(false);
+
+function answerDisplay() {
+  if (show.value) {
+    return MainAnswerAnswerDisplay;
+  }
+  return MainAnswerQuestionDisplay;
+}
+</script>
+
+<style scoped>
+.count-container {
+  border-radius: 20px 20px 0 0;
+  text-align: center;
+}
+</style>
