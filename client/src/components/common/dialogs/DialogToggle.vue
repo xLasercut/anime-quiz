3ae245edbb5321747a1c2cdb937e6b6c819019e3
Tim Wang <tianxun.wang2004@gmail.com>
2024-01-12 10:03:10 +0000

    changed radio to select component (#71)

---
+++ client/src/components/common/dialogs/DialogToggle.vue
@@ -0,0 +1,12 @@
+<template>
+  <dialog-select v-bind="$attrs" :items="items" item-value="value" item-title="text"></dialog-select>
+</template>
+
+<script setup lang="ts">
+import DialogSelect from '@/components/common/dialogs/DialogSelect.vue';
+
+const items = [
+  { text: 'True', value: true },
+  { text: 'False', value: false }
+];
+</script>
