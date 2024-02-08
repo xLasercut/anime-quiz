bde8dc08af6066ba809402045130e67ef13a6c96
xLasercut <tianxun.wang2004@gmail.com>
2024-02-08 20:06:08 +0000

    Deploying to gh-pages from @ xLasercut/anime-quiz@527cf636dc4c8eb1a6f9e7311e30c9e03c1fee35 🚀

---
+++ assets/vidstack-audio-c6404aac.js
@@ -0,0 +1,1 @@
+import{H as t}from"./vidstack-html-925e1d32.js";import"./index-c9f48ec7.js";import"./vidstack-XrFM82kz-43520f6f.js";class o extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(){super.setup(),this.type==="audio"&&this.b.delegate.c("provider-setup",this)}get audio(){return this.a}}export{o as AudioProvider};
