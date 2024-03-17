bf89f5f0e6a5861133769ca92c6f589240a04f12
xLasercut <tianxun.wang2004@gmail.com>
2024-03-17 09:14:57 +0000

    Deploying to gh-pages from @ xLasercut/anime-quiz@8d9c4fa80258e586f5fe6abe9b15a435f49c2e43 🚀

--- assets/vidstack-BhYx9Fjk-10a6a705.js
+++
@@ -1,1 +0,0 @@
-import{g as i,J as t}from"./index-9aae1eb5.js";class r{constructor(s){this.Ca=s}Bb(){i(this.ca)&&this.Oe()}ra(){t(this.ca)&&window.cancelAnimationFrame(this.ca),this.ca=void 0}Oe(){this.ca=window.requestAnimationFrame(()=>{i(this.ca)||(this.Ca(),this.Oe())})}}export{r as R};
---
+++ assets/vidstack-audio-98bafd74.js
@@ -0,0 +1,1 @@
+import{H as t}from"./vidstack-html-2c4916ee.js";import"./index-b30f2182.js";import"./vidstack-BhYx9Fjk-53d8cf7f.js";class o extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(){super.setup(),this.type==="audio"&&this.b.delegate.c("provider-setup",this)}get audio(){return this.a}}export{o as AudioProvider};
