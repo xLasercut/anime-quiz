806db4d36e523e301de51f2e1467110caace464a
xLasercut <tianxun.wang2004@gmail.com>
2024-06-07 14:33:21 +0000

    Deploying to gh-pages from @ xLasercut/anime-quiz@6175b9728006443e684c224954f4bf1691811fd7 🚀

--- assets/BulkAddSongsPanel-Ut0M74gC.js
+++
@@ -1,1 +0,0 @@
-import{d as o,c as t,w as s,x as r,b as c,E as u,e as i,l}from"./index-BAK_NbG2.js";const k=o({__name:"BulkAddSongsPanel",setup(p){const a=c();function n(){a.push(u.LOBBY)}return(_,e)=>(i(),t(r,{icon:"mdi-backspace-reverse-outline",color:"warning",onClick:e[0]||(e[0]=d=>n())},{default:s(()=>[l("Back")]),_:1}))}});export{k as default};
