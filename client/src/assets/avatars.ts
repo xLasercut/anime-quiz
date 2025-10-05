46fae93aaf7dabe5f18de9227f20143141377c62
Tim Wang <tianxun.wang2004@gmail.com>
2025-10-05 19:52:49 +0100

    moved avatar image urls to github (#312)

--- client/src/assets/avatars.ts
+++ client/src/assets/avatars.ts
@@ -1,28 +1,30 @@
 import { AVATARS } from 'anime-quiz-shared-resources';
 
+const AVATAR_IMG_BASE_URL = 'https://raw.githubusercontent.com/xLasercut/server-images/master/amq-avatars';
+
 const AVATAR_MAP = {
-  [AVATARS.ZERO_TWO]: 'https://i.imgur.com/qQ0Fkkx.png',
-  [AVATARS.INITIAL_D]: 'https://i.imgur.com/fk44rTD.png',
-  [AVATARS.MISAKA]: 'https://i.imgur.com/eZ6FZcr.png',
-  [AVATARS.EVA_UNIT_1]: 'https://i.imgur.com/aqld5ou.png',
-  [AVATARS.HOMURA]: 'https://i.imgur.com/08X3kjC.png',
-  [AVATARS.ALPHONSE]: 'https://i.imgur.com/PerwiGF.png',
-  [AVATARS.HORO]: 'https://i.imgur.com/oOiDAFl.png',
-  [AVATARS.MADOKA]: 'https://i.imgur.com/iGPVIAN.png',
-  [AVATARS.LELOUCH]: 'https://i.imgur.com/mB0IKty.png',
-  [AVATARS.MIYU]: 'https://i.imgur.com/Jvi0FoN.png',
-  [AVATARS.RAWR]: 'https://i.imgur.com/t03jCAX.png',
-  [AVATARS.PIKACHU]: 'https://i.imgur.com/QoEzMPR.jpg',
-  [AVATARS.EREN]: 'https://i.imgur.com/5Dip9VY.jpg',
-  [AVATARS.JUDAI]: 'https://i.imgur.com/IpCOoWU.png',
-  [AVATARS.YUGI]: 'https://i.imgur.com/JUdFemG.png',
-  [AVATARS.REG]: 'https://i.imgur.com/aOSK1uc.png',
-  [AVATARS.NANACHI]: 'https://i.imgur.com/lbQa8Ru.png',
-  [AVATARS.SHADOW]: 'https://i.imgur.com/rP10lTt.png',
-  [AVATARS.ANYA]: 'https://i.imgur.com/RXX3z5Q.png',
-  [AVATARS.SYLPHIE]: 'https://i.imgur.com/kGnKKjT.png',
-  [AVATARS.ROXY]: 'https://i.imgur.com/UtqhYfC.png',
-  [AVATARS.ERIS]: 'https://i.imgur.com/YtCBX1c.png'
+  [AVATARS.ZERO_TWO]: `${AVATAR_IMG_BASE_URL}/zero-two.png`,
+  [AVATARS.INITIAL_D]: `${AVATAR_IMG_BASE_URL}/ae86.png`,
+  [AVATARS.MISAKA]: `${AVATAR_IMG_BASE_URL}/misaka.png`,
+  [AVATARS.EVA_UNIT_1]: `${AVATAR_IMG_BASE_URL}/eva-unit-1.png`,
+  [AVATARS.HOMURA]: `${AVATAR_IMG_BASE_URL}/homura.png`,
+  [AVATARS.ALPHONSE]: `${AVATAR_IMG_BASE_URL}/alphonse.png`,
+  [AVATARS.HORO]: `${AVATAR_IMG_BASE_URL}/holo.png`,
+  [AVATARS.MADOKA]: `${AVATAR_IMG_BASE_URL}/madoka.png`,
+  [AVATARS.LELOUCH]: `${AVATAR_IMG_BASE_URL}/lelouch.png`,
+  [AVATARS.MIYU]: `${AVATAR_IMG_BASE_URL}/miku.png`,
+  [AVATARS.RAWR]: `${AVATAR_IMG_BASE_URL}/taiga.png`,
+  [AVATARS.PIKACHU]: `${AVATAR_IMG_BASE_URL}/pikachu.jpg`,
+  [AVATARS.EREN]: `${AVATAR_IMG_BASE_URL}/eren.jpg`,
+  [AVATARS.JUDAI]: `${AVATAR_IMG_BASE_URL}/judai.png`,
+  [AVATARS.YUGI]: `${AVATAR_IMG_BASE_URL}/yugi.png`,
+  [AVATARS.REG]: `${AVATAR_IMG_BASE_URL}/reg.png`,
+  [AVATARS.NANACHI]: `${AVATAR_IMG_BASE_URL}/nanachi.png`,
+  [AVATARS.SHADOW]: `${AVATAR_IMG_BASE_URL}/shadow.png`,
+  [AVATARS.ANYA]: `${AVATAR_IMG_BASE_URL}/anya.png`,
+  [AVATARS.SYLPHIE]: `${AVATAR_IMG_BASE_URL}/sylphie.png`,
+  [AVATARS.ROXY]: `${AVATAR_IMG_BASE_URL}/roxy.png`,
+  [AVATARS.ERIS]: `${AVATAR_IMG_BASE_URL}/eris.png`
 };
 
-export { AVATAR_MAP };
+export { AVATAR_MAP, AVATAR_IMG_BASE_URL };
